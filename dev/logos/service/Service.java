f7429b9b6f66641c253118eee97ea0efc316fac0
Michael Trinque <michael@logos.dev>
2024-10-02 17:16:54 -0500

    add Service.onFailedRequest
    Helper method to log an error and return an error response to the
    calling client.

--- dev/logos/service/Service.java
+++ dev/logos/service/Service.java
@@ -5,13 +5,32 @@ import dev.logos.user.User;
 import dev.logos.user.UserContext;
 import io.grpc.BindableService;
 import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
+import java.util.logging.Level;
 
 public interface Service extends BindableService {
+    Logger logger = LoggerFactory.getLogger(Service.class);
+
     default <Req> boolean allow(Req request, User user) { return false; }
 
     default <Req> void validate(Req request, Validator validator) { }
+
+    default void onFailedRequest(
+            StreamObserver<?> responseObserver,
+            String msg,
+            Object obj
+    ) {
+
+        logger.atError().log(msg, obj);
+        responseObserver.onError(
+                Status.INVALID_ARGUMENT
+                        .withDescription(msg)
+                        .asRuntimeException());
+    }
 
     default <Req> Optional<Status> guard(Req request) {
         if (!allow(request, UserContext.getCurrentUser())) {
