623d911adf054fd66404d4303efdcaa912a4a068
Michael Trinque <michael@logos.dev>
2025-06-03 21:31:08 -0500

    use cached DateTimeFormatter in TimestampMapper

--- dev/logos/service/storage/pg/exporter/codegen/type/TimestampMapper.java
+++ dev/logos/service/storage/pg/exporter/codegen/type/TimestampMapper.java
@@ -4,6 +4,7 @@ import com.squareup.javapoet.CodeBlock;
 import dev.logos.service.storage.pg.exporter.mapper.PgTypeMapper;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.util.List;
@@ -14,6 +15,9 @@ public class TimestampMapper extends PgTypeMapper {
     public TimestampMapper() {
         super(List.of("pg_catalog.timestamptz", "pg_catalog.timestamp"), TYPE_STRING, "getString");
     }
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            getDateTimeFormatterBuilder().toFormatter();
 
     public static DateTimeFormatterBuilder getDateTimeFormatterBuilder() {
         return new DateTimeFormatterBuilder()
@@ -26,13 +30,9 @@ public class TimestampMapper extends PgTypeMapper {
     public CodeBlock protoToPg(String queryVariable, String dbField, String protoVariable, String protoGetter) {
         return CodeBlock.builder()
                         .beginControlFlow("if (!$L.$L().isEmpty())", protoVariable, protoGetter)
-                        .add("$L.bind($S, $T.parse((String) $L.$L(), new $T()" +
-                                        ".appendPattern(\"yyyy-MM-dd HH:mm:ss\")" +
-                                        ".appendFraction($T.MICRO_OF_SECOND, 0, 6, true)" +
-                                        ".appendPattern(\"x\")" +
-                                        ".toFormatter()));",
+                        .add("$L.bind($S, $T.parse((String) $L.$L(), $T.DATE_TIME_FORMATTER));",
                                 queryVariable, dbField, OffsetDateTime.class, protoVariable, protoGetter,
-                                DateTimeFormatterBuilder.class, ChronoField.class)
+                                TimestampMapper.class)
                         .nextControlFlow("else")
                         .add("$L.bind($S, ($T) null);", queryVariable, dbField, OffsetDateTime.class)
                         .endControlFlow()
